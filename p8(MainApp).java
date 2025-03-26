73447fedf97cc0f0f5b4617813a704dbcf1b6de5
Ayanokoji4328 <guptaabhi0520@gmail.com>
2025-03-26 20:08:36 +0530

    8th practical

---
+++ p8(MainApp).java
@@ -0,0 +1,8 @@
+//File: MainApp.java
+import mypackage.MyPackageClass;
+public class MainApp {
+    public static void main(String[] args) {
+        MyPackageClass obj = new MyPackageClass();
+        obj.displayMessage();
+    }
+}
---
+++ p8(MypackageClass).java
@@ -0,0 +1,7 @@
+//File: MyPackageClass.java (inside 'mypackage' folder)
+package mypackage;
+public class MyPackageClass {
+    public void displayMessage() {
+        System.out.println("Hello from MyPackageClass in mypackage!");
+    }
+}
